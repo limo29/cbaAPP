1b28b2f507883e525fb36fd65aedd20b4f881cf3
limo29 <eliaslay29@gmail.com>
2026-01-09 16:03:29 +0100

    driver layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="de">
+    <html lang="de" suppressHydrationWarning>
       <body className={inter.className}>
         <div style={{ paddingTop: '64px' }}>
           {children}
